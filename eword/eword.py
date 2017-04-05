360542fe76a6873de052878a5e4d261de307c955
Nori Ohara <hideno0110@gmail.com>
2017-04-05 23:37:29 +0900

    for deploy

--- eword/eword.py
+++ eword/eword.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('eword/')
 import treetaggerwrapper
+# from flask.ext.sqlalchemy import SQLAlchemy
 import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
